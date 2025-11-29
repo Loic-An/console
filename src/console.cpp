28fb97796eb20850c779c0705d6f31387c6c372d
Loïc André <loic.andre00@gmail.com>
2025-11-29 22:12:51 +0100

    basic cursor movement and signal handling

---
+++ src/console.cpp
@@ -0,0 +1,137 @@
+#include "console.hpp"
+#include <csignal>
+
+static volatile sig_atomic_t g_notKilled = 1;
+void sig_handler(int sig)
+{
+    (void)sig;
+    g_notKilled = 0;
+}
+Console::~Console()
+{
+    this->write("endofline");
+    // Destructor implementation
+}
+
+Size Console::getSize()
+{
+    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &this->size) == -1)
+    {
+        perror("ioctl");
+    }
+    return {this->size.ws_col, this->size.ws_row};
+}
+
+void Console::moveCursorUp(const int &n = 1)
+{
+    if (n > 0)
+        this->interface << "\033[" << n << "A";
+}
+void Console::moveCursorDown(const int &n = 1)
+{
+    if (n > 0)
+        this->interface << "\033[" << n << "B";
+}
+void Console::moveCursorRight(const int &n = 1)
+{
+    if (n > 0)
+        this->interface << "\033[" << n << "C";
+}
+void Console::moveCursorLeft(const int &n = 1)
+{
+    if (n > 0)
+        this->interface << "\033[" << n << "D";
+}
+
+void Console::clearLine()
+{
+}
+void Console::clearStart()
+{
+    this->interface << "\033[0J";
+}
+void Console::clearEnd()
+{
+    this->interface << "\033[1J";
+}
+
+void Console::clearScreen()
+{
+    this->interface << "\033[2J";
+}
+
+void Console::cursorPos(const int &row = 1, const int &col = 1)
+{
+    if (row > 0 && col > 0)
+        this->interface << "\033[" << row << ";" << col << "H";
+}
+
+void Console::write(const Size &data)
+{
+    this->interface << "Size<col=" << data.col << ";row=" << data.row << ">";
+}
+template <typename OStreamWritable>
+inline void Console::write(const OStreamWritable &data)
+{
+    this->interface << data;
+}
+
+int Console::mainLoop()
+{
+    while (g_notKilled)
+    {
+        for (auto fun : this->funvector)
+            fun(this);
+        this->interface << std::flush;
+    }
+    std::cout << "mainloopend";
+    return 0;
+}
+
+Console::Console(std::ostream &interface = std::cout, const bool nosetup = false) : interface(interface)
+{
+    // Constructor implementation
+    // Bind internal streambuf to the provided stream to avoid assigning std::ostream (assignment is deleted)
+    this->getSize(); // feed size struct with initial data
+    if (nosetup)
+        return;
+    // interface.rdbuf()->pubsetbuf(0, 0); // no buffer
+    for (int i = 1; i < this->size.ws_row; i++)
+    {
+        interface << "\n";
+    }
+    this->cursorPos(1, 1);
+    this->funvector = {[](Console *c)
+                       {
+                           (void)c->getSize();
+                           // std::cout << "1";
+                       }};
+}
+
+Console c;
+int main()
+{
+    std::signal(SIGINT, sig_handler);
+    std::signal(SIGTERM, sig_handler);
+    std::signal(SIGWINCH, [](int sig)
+                {
+                    c.write(c.getSize());
+                std::cout <<"new size"<<sig<<std::endl; });
+    c.write(c.getSize());
+    c.moveCursorRight(15);
+    c.moveCursorDown(2);
+    c.moveCursorDown();
+    c.write("hello");
+    c.write("\033[30;107mB");
+    c.write("\033[0mHELLO");
+    // c.clearScreen();
+    /*
+        for (int i = 0; i < s.col; i++)
+        {
+            c << "0";
+            c.moveCursorLeft();
+            c.moveCursorUp();
+        }
+        c << "\n";*/
+    return c.mainLoop();
+}
---
+++ src/console.hpp
@@ -0,0 +1,59 @@
+#if defined(__CYGWIN__) || defined(__unix__) || defined(__APPLE__)
+// POSIX-like platforms (Cygwin, Unix, macOS)
+#include <csignal>
+#include <sys/ioctl.h>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+
+struct Size
+{
+    int col;
+    int row;
+};
+
+enum Color
+{
+    BLACK = 30,
+    RED = 31,
+    GREEN = 32,
+    YELLOW = 33,
+    BLUE = 34,
+    MAGENTA = 35,
+    CYAN = 36,
+    WHITE = 37
+};
+
+class Console
+{
+private:
+    winsize size;
+    Size pos;
+    std::ostream &interface;
+    std::vector<void (*)(Console *)> funvector;
+
+public:
+    Console(std::ostream &interface, const bool nosetup);
+    ~Console();
+    Size getSize();
+    void moveCursorUp(const int &n);
+    void moveCursorDown(const int &n);
+    void moveCursorRight(const int &n);
+    void moveCursorLeft(const int &n);
+    void clearLine();
+    void clearStart();
+    void clearEnd();
+    void clearScreen();
+    void cursorPos(const int &row, const int &col);
+    void write(const Size &data);
+    template <typename OStreamWritable>
+    void write(const OStreamWritable &data);
+    int mainLoop();
+};
+
+#elif defined(_WIN32)
+// Windows platforms
+#error no native Windows support. Use Cygwin. Compilation aborted.
+#else
+#error "Unknown platform: compilation aborted"
+#endif
